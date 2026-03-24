9aa01060f4c6888257e3696c7606ebde3787b582
juwonhs0913 <juwon0913@soongsil.ac.kr>
2026-03-24 17:01:01 +0900

    17:00ver

--- User.cpp
+++ User.cpp
@@ -1,3 +1,16 @@
 #include "Movie.h"
 #include <iostream>
 
+User::User()
+    : id(0) {}
+
+User::User(int id, const std::string& name, const std::string& email)
+    : id(id), name(name), email(email) {}
+
+int User::getId() const {return id; }
+std::string User::getName() const {return name; }
+std::string User::getEmail() const {return email; }
+
+void User::display() const {
+    //std::cout << id 
+}
